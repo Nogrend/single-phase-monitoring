044666f77c11932b2adfc2e18d0aef25d90979b7
bram <nogrend@msn.com>
2024-01-01 15:04:06 +0100

    module phase meter

---
+++ src/phase_meter/__init__.py
@@ -0,0 +1,1 @@
+from .phase_meter import PhaseMeter
---
+++ src/phase_meter/phase_meter.py
@@ -0,0 +1,17 @@
+from typing import Dict
+
+import os
+import sdm_modbus
+
+
+class PhaseMeter:
+    def __init__(self):
+        self.phaseMeter = sdm_modbus.SDM120(device=os.getenv("COM_PORT"),
+                                            unit=os.getenv("MODBUS_ADDRESS"),
+                                            baud=int(os.getenv("BAUD_RATE")),
+                                            parity=os.getenv("PARITY"))
+
+    def get_measurement(self) -> Dict:
+        if not self.device.connected():
+            return {}
+        return self.device.read_all(sdm_modbus.registerType.INPUT)
