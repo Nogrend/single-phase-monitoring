96ab6be65fe7079b1f4c2679b9508f2407848316
bram <nogrend@msn.com>
2024-01-01 17:11:40 +0100

    debug

--- src/phase_meter/phase_meter.py
+++ src/phase_meter/phase_meter.py
@@ -6,11 +6,6 @@ import sdm_modbus
 
 class PhaseMeter:
     def __init__(self):
-        # self.device = sdm_modbus.SDM120(device="/dev/ttyUSB0",
-        #                                 unit=1,
-        #                                 baud=9600,
-        #                                 parity="N")
-
         self.device = sdm_modbus.SDM120(device=os.getenv("COM_PORT"),
                                         unit=int(os.getenv("MODBUS_ADDRESS")),
                                         baud=int(os.getenv("BAUD_RATE")),
@@ -20,6 +15,4 @@ class PhaseMeter:
         if not self.device.connected():
             return {"status": "not connected"}
         results = self.device.read_all(sdm_modbus.registerType.INPUT)
-        print(results)
         return results
-        # return self.device.read_all(1) # sdm_modbus.registerType.INPUT
