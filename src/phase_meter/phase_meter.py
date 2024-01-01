84b75bde545e539966bd816008b7ef0447efff4d
bram <nogrend@msn.com>
2024-01-01 15:57:45 +0100

    fix bug

--- src/phase_meter/phase_meter.py
+++ src/phase_meter/phase_meter.py
@@ -7,9 +7,9 @@ import sdm_modbus
 class PhaseMeter:
     def __init__(self):
         self.device = sdm_modbus.SDM120(device=os.getenv("COM_PORT"),
-                                            unit=os.getenv("MODBUS_ADDRESS"),
-                                            baud=int(os.getenv("BAUD_RATE")),
-                                            parity=os.getenv("PARITY"))
+                                        unit=os.getenv("MODBUS_ADDRESS"),
+                                        baud=int(os.getenv("BAUD_RATE")),
+                                        parity=os.getenv("PARITY"))
 
     def get_measurement(self) -> Dict:
         if not self.device.connected():
