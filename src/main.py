633180f8c83fcad0a46aa96e0238b28a44032da9
bram <nogrend@msn.com>
2024-01-10 22:09:34 +0100

    add uuid to message

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from time import sleep
 from datetime import datetime
+import uuid
 
 from dotenv import load_dotenv
 from phase_meter import PhaseMeter
@@ -16,7 +17,10 @@ if __name__ == "__main__":
     is_running: bool = True
     while is_running:
         measurement = phase_meter.get_measurement()
+        measurement["id"] = uuid.uuid4()
         measurement["timestamp"] = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        if measurement == {}:
+            measurement = {"status": "no results"}
         client_2_broker.sent_to_broker(measurement)
         sleep(60)
         # is_running = False
