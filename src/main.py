1688a512ffcd5d05be3d0f83a0bc40406be83fd2
bram <nogrend@msn.com>
2024-01-14 11:24:02 +0100

    fix wrong payload

--- src/main.py
+++ src/main.py
@@ -10,19 +10,23 @@ if __name__ == "__main__":
     phase_meter = PhaseMeter()
     mqtt = Client2Broker()
 
+    payload = mqtt.build_payload_with_id_and_timestamp({"message": "starting script"})
+    mqtt.sent_to_broker(payload, Client2Broker.AllowedTopics.STATUS)
+
     is_running: bool = True
     while is_running:
         measurement = phase_meter.get_measurement()
 
         if measurement == {}:
             content = ""
-            payload = mqtt.build_payload_with_id_and_timestamp({"message":"no data received"})
-            mqtt.sent_to_broker(measurement, Client2Broker.AllowedTopics.STATUS)
+            payload = mqtt.build_payload_with_id_and_timestamp({"message": "no data received"})
+            mqtt.sent_to_broker(payload, Client2Broker.AllowedTopics.STATUS)
 
         else:
             payload = mqtt.build_payload_with_id_and_timestamp(measurement)
-            mqtt.sent_to_broker(measurement, Client2Broker.AllowedTopics.MEASUREMENT_ALL)
+            mqtt.sent_to_broker(payload, Client2Broker.AllowedTopics.MEASUREMENT_ALL)
 
         sleep(60)
 
-
+    payload = mqtt.build_payload_with_id_and_timestamp({"message": "script down"})
+    mqtt.sent_to_broker(payload, Client2Broker.AllowedTopics.STATUS)
